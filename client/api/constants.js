479ae2e1a94b22454cb5107499a558e8124307bc
fraoucene <fraoucene.guezout@gmail.com>
2017-02-20 14:09:33 +0100

    Update constants.js

--- client/api/constants.js
+++ client/api/constants.js
@@ -4,7 +4,7 @@ export const API_PUBLIC = "298bab46381a6daaaee19aa5c8cafea5";
 export const API_PRIVATE = "b0223681fced28de0fe97e6b9cd091dd36a5b71d";
 export const TIME_STAMPE_VALUE = + new Date();
 
-export const BASE_URL = "http://gateway.marvel.com:80";
+export const BASE_URL = "https://gateway.marvel.com:80";
 export const HEROS_URL = "/v1/public/characters";
 
 export const LIMIT_KEY = "limit";
